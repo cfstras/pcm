4f4e1a460f32aceecd03712fd426d77a09fbe903
Claus Strasburger <claus@stasburger.de>
2015-10-25 22:12:40 -0600

    build for static and dynamic

--- buildall.sh
+++ buildall.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-BUILD = "go build -v"
-STATIC = '-ldflags -extldflags="-static"'
-
 rm -rfv build/
 export CGO_ENABLED=1
 
@@ -10,13 +7,14 @@ echo "---"
 export GOOS=linux
 export GOARCH=386
 go env
-go build -v -ldflags -extldflags="-static" -o build/linux-386/pcm || exit 1
+go build -v -ldflags -extldflags="-static" -o build/linux-386/pcm.static-linux-386 || exit 1
+go build -v -o build/linux-386/pcm.linux-386
 
 echo "---"
 export GOOS=linux
 export GOARCH=amd64
 go env
-go build -v -ldflags -extldflags="-static" -o build/linux-amd64/pcm || exit 1
-
+go build -v -ldflags -extldflags="-static" -o build/linux-amd64/pcm.static-linux-amd64 || exit 1
+go build -v -o build/linux-amd64/pcm.linux-amd64
 
 find build -type f -exec file {} \;
