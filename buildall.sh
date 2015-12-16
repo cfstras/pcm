b2b1212f13944a106d24926b581c07571f045115
Claus Strasburger <claus@strasburger.de>
2015-12-16 11:22:11 +0100

    travis build osx && artifacts deployment

--- buildall.sh
+++ buildall.sh
@@ -1,20 +1,36 @@
 #!/bin/bash
 
+set -o pipefail
+set -e
+
 rm -rfv build/
 export CGO_ENABLED=1
+
+if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
+    export GOARCH=amd64
+    go env
+    go build -v -o build/osx-x64/pcm
+
+    export GOARCH=386
+    go env
+    go build -v -o build/osx-x86/pcm
+
+    find build -type f -exec file {} \;
+    exit
+fi
 
 echo "---"
 export GOOS=linux
 export GOARCH=386
 go env
-go build -v -ldflags -extldflags="-static" -o build/linux-386/pcm.static-linux-386 || exit 1
-go build -v -o build/linux-386/pcm.linux-386
+go build -v -ldflags -extldflags="-static" -o build/linux-x86/static/pcm
+go build -v -o build/linux-x86/dynamic/pcm
 
 echo "---"
 export GOOS=linux
 export GOARCH=amd64
 go env
-go build -v -ldflags -extldflags="-static" -o build/linux-amd64/pcm.static-linux-amd64 || exit 1
-go build -v -o build/linux-amd64/pcm.linux-amd64
+go build -v -ldflags -extldflags="-static" -o build/linux-x64/static/pcm
+go build -v -o build/linux-x64/dynamic/pcm
 
 find build -type f -exec file {} \;
