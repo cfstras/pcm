b628cafb8613e9e1fe6e4894b48d593d6ca97838
Claus Strasburger <claus@strasburger.de>
2017-06-17 13:54:28 -0600

    updated deps

---
+++ vendor/github.com/cfstras/go-utils/fileutil/main.go
@@ -0,0 +1,34 @@
+package fileutil
+
+import (
+	"bufio"
+	"os"
+)
+
+func Exists(path string) (bool, error) {
+	_, err := os.Stat(path)
+	if err == nil {
+		return true, nil
+	}
+	if os.IsNotExist(err) {
+		return false, nil
+	}
+	return true, err
+}
+
+// readLines reads a whole file into memory
+// and returns a slice of its lines.
+func ReadLines(path string) ([]string, error) {
+	file, err := os.Open(path)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+
+	var lines []string
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		lines = append(lines, scanner.Text())
+	}
+	return lines, scanner.Err()
+}
